0d325addf0a7199db728fb400843685227c81a2e
yuanxiao <btsoUA01&Wb0R$U8RbrxajI5KPhbFKLN>
2022-09-19 18:59:31 +0800

    webmvc启动源码

--- demo-spring-mvc/src/main/java/com/demo2/config/MyWebAppInitializer.java
+++ demo-spring-mvc/src/main/java/com/demo2/config/MyWebAppInitializer.java
@@ -11,57 +11,57 @@ import javax.servlet.ServletRegistration;
  * @description MyWebAppInitializer
  * @date 2022/9/19
  */
-public class MyWebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+public class MyWebAppInitializer  {
 
-	/**
-	 * 父容器配置类
-	 */
-	@Override
-	protected Class<?>[] getRootConfigClasses() {
-		return new Class[]{RootConfig.class};
-	}
-
-	/**
-	 * 子容器配置类
-	 */
-	@Override
-	protected Class<?>[] getServletConfigClasses() {
-		return new Class[]{WebConfig.class};
-	}
-
-	/**
-	 * DispatchServlet 路径映射
-	 */
-	@Override
-	protected String[] getServletMappings() {
-		return new String[]{"/"};
-	}
-
-	/**
-	 * 父容器刷新前的回调
-	 */
-	@Override
-	protected ApplicationContextInitializer<?>[] getRootApplicationContextInitializers() {
-		return new ApplicationContextInitializer[]{applicationContext -> {
-			System.out.println("收到 RootApplicationContextInitializers");
-		}};
-	}
-
-	/**
-	 * 子容器初始化前的回调
-	 */
-	@Override
-	protected ApplicationContextInitializer<?>[] getServletApplicationContextInitializers() {
-		return new ApplicationContextInitializer[]{applicationContext -> {
-			System.out.println("收到 ServletApplicationContextInitializers");
-		}};
-	}
-
-	/**
-	 * 注册 DispatcherServlet 的最后修改回调
-	 */
-	@Override
-	protected void customizeRegistration(@NonNull ServletRegistration.Dynamic registration) {
-		System.out.println(registration);
-	}
+//	/**
+//	 * 父容器配置类
+//	 */
+//	@Override
+//	protected Class<?>[] getRootConfigClasses() {
+//		return new Class[]{RootConfig.class};
+//	}
+//
+//	/**
+//	 * 子容器配置类
+//	 */
+//	@Override
+//	protected Class<?>[] getServletConfigClasses() {
+//		return new Class[]{WebConfig.class};
+//	}
+//
+//	/**
+//	 * DispatchServlet 路径映射
+//	 */
+//	@Override
+//	protected String[] getServletMappings() {
+//		return new String[]{"/"};
+//	}
+//
+//	/**
+//	 * 父容器刷新前的回调
+//	 */
+//	@Override
+//	protected ApplicationContextInitializer<?>[] getRootApplicationContextInitializers() {
+//		return new ApplicationContextInitializer[]{applicationContext -> {
+//			System.out.println("收到 RootApplicationContextInitializers");
+//		}};
+//	}
+//
+//	/**
+//	 * 子容器初始化前的回调
+//	 */
+//	@Override
+//	protected ApplicationContextInitializer<?>[] getServletApplicationContextInitializers() {
+//		return new ApplicationContextInitializer[]{applicationContext -> {
+//			System.out.println("收到 ServletApplicationContextInitializers");
+//		}};
+//	}
+//
+//	/**
+//	 * 注册 DispatcherServlet 的最后修改回调
+//	 */
+//	@Override
+//	protected void customizeRegistration(@NonNull ServletRegistration.Dynamic registration) {
+//		System.out.println(registration);
+//	}
 }
